98a820a61eb3cc245674164535713e98631e8934
Daniel de jesus <dani-wissars-13@live.com>
2020-12-11 12:01:59 -0600

    Ocultar Credenciales clientes de swagger

--- Controllers/CredencialesClientesController.cs
+++ Controllers/CredencialesClientesController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiCargaDocsFormaliza.Controllers
 {
+    [ApiExplorerSettings(IgnoreApi = true)]
     [Route("api/[controller]")]
     [ApiController]
     public class CredencialesController : ControllerBase
